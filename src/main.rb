dc57c02974d3682cbd6566543c86c99f161f3429
dev <dev.aravind@culturecreates.com>
2024-01-25 18:21:30 +0530

    Added sparqls and unit test to fix data

--- src/main.rb
+++ src/main.rb
@@ -8,14 +8,21 @@ if ARGV.length < 4
   exit
 end
 
-page_url = ARGV[0]
-entity_identifier = ARGV[1]
-file_name = ARGV[2]
-is_paginated = ARGV[3]
-base_url = ARGV[4] || ''
+def perform_sparql_transformations(graph, sparql_paths)
+  sparql_paths.each do |sparql_path|
+    graph.query(SPARQL.parse(File.read(sparql_path), update: true))
+  end
+  return graph
+end
+
+page_url, entity_identifier, file_name, is_paginated, base_url = ARGV[0..4]
+base_url = '' if base_url == nil
 max_retries, retry_count = 3, 0
 page_number = is_paginated == 'true' ? 1 : nil
 graph = RDF::Graph.new
+add_url_sparql_file = File.read('./sparql/add_derived_from.sparql')
+
+entity_urls = []
 
 loop do
   url = "#{page_url}#{page_number}"
@@ -33,36 +40,39 @@ loop do
 
   main_doc = Nokogiri::HTML(main_page_html_text)
   entities_data = main_doc.css(entity_identifier)
-  entity_urls = []
+  number_of_entities = entity_urls.length
   entities_data.each do |entity|
     entity_urls << base_url+entity['href']
   end
-  if entity_urls.empty?
+  if entity_urls.length == number_of_entities
     puts "No more entities found on page #{page_number}. Exiting..."
     break
   end
 
-  entity_urls.each do |entity_url|
-    begin
-      entity_url = entity_url.gsub(' ', '+')
-      graph << RDF::Graph.load(entity_url)
-    rescue StandardError => e
-      puts "Error loading RDF from #{entity_url}: #{e.message}"
-      break
-    end
-  end
-  if page_number == nil
+  if page_number == 1
     break
   else
     page_number += 1
   end
   retry_count = 0
 end
+entity_urls.uniq!
+entity_urls.each do |entity_url|
+  begin
+    entity_url = entity_url.gsub(' ', '+')
+    loaded_graph = RDF::Graph.load(entity_url)
+    sparql_file_with_url = add_url_sparql_file.gsub("subject_url", entity_url)
+    loaded_graph.query(SPARQL.parse(sparql_file_with_url, update: true))
+    graph << loaded_graph
+  rescue StandardError => e
+    puts "Error loading RDF from #{entity_url}: #{e.message}"
+    break
+  end
+end
 
-replace_blank_nodes_sparql = "./sparql/replace_blank_nodes.sparql"
-fix_entity_type_sparql = "./sparql/fix_entity_type.sparql"
-graph.query(SPARQL.parse(File.read(replace_blank_nodes_sparql), update: true))
-graph.query(SPARQL.parse(File.read(fix_entity_type_sparql), update: true))
+sparql_paths = ["./sparql/replace_blank_nodes.sparql", "./sparql/fix_entity_type.sparql", "./sparql/fix_date.sparql"]
+graph = perform_sparql_transformations(graph, sparql_paths)
+
 File.open(file_name, 'w') do |file|
   file.puts(graph.dump(:jsonld))
 end
--- tests/test.rb
+++ tests/test.rb
@@ -6,6 +6,7 @@ class SparqlTest < Minitest::Test
   def setup
     @fix_entity_type_sparql_file = "./sparql/fix_entity_type.sparql"
     @replace_blank_node_sparql_file = "./sparql/replace_blank_nodes.sparql"
+    @fix_date_sparql_file = './sparql/fix_date.sparql'
   end
 
   # check that the type object is fixed
@@ -30,4 +31,18 @@ class SparqlTest < Minitest::Test
     # puts "after: #{graph.dump(:jsonld)}"
     assert_equal false, graph.query([nil, RDF::type, RDF::URI("http://schema.org/Thing")]).each.subjects.node?
   end
+
+  # check that the dates are correctly formatted
+  def test_date_format
+    sparql = SPARQL.parse(File.read(@fix_date_sparql_file), update: true)
+    graph = RDF::Graph.load("./tests/fixtures/test_date.jsonld")
+    # puts "before: #{graph.dump(:jsonld)}"
+    graph.query(sparql)
+    # puts "after: #{graph.dump(:jsonld)}"
+    dates = graph.query([nil, RDF::URI("http://schema.org/startDate"), nil]).each.objects
+    dates.each do |start_date|
+      assert_match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}\s*$/, start_date.to_s)
+    end
+  end
+
 end
