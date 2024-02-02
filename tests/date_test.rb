9da899cc3b26b1d53a19725a42948c17afdf779d
dev <dev.aravind@culturecreates.com>
2024-02-02 17:44:59 +0530

    Added unit test for levivier dates

--- tests/date_test.rb
+++ tests/date_test.rb
@@ -14,9 +14,22 @@ class DateTest < Minitest::Test
     # puts "before: #{graph.dump(:jsonld)}"
     graph.query(sparql)
     # puts "after: #{graph.dump(:jsonld)}"
-    dates = graph.query([nil, RDF::URI("http://schema.org/startDate"), nil]).each.objects
+    dates = graph.query([nil, RDF::Vocab::SCHEMA.startDate, nil]).each.objects
     dates.each do |start_date|
       assert_match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}\s*$/, start_date.to_s)
+    end
+  end
+
+  def test_date_type
+    sparql = SPARQL.parse(File.read(@fix_date_sparql_file), update: true)
+    graph = RDF::Graph.load("./tests/fixtures/test_date_types.jsonld")
+    # puts "before: #{graph.dump(:jsonld)}"
+    graph.query(sparql)
+    # puts "after: #{graph.dump(:jsonld)}"
+    events = JSON.parse(graph.dump(:jsonld))
+    events.each do |event|
+      start_date = event['http://schema.org/startDate']
+      assert_equal(start_date[0]["@type"], "http://schema.org/Date")
     end
   end
 end
