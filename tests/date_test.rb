9738f6bc2e2a73abfd74c981327758276ac30184
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-04 21:53:23 -0500

    added new test

--- tests/date_test.rb
+++ tests/date_test.rb
@@ -4,15 +4,15 @@ require 'linkeddata'
 class DateTest < Minitest::Test
 
   def setup
-    @fix_date_sparql_file = './sparql/fix_date.sparql'
+    fix_date_sparql_file = './sparql/fix_date.sparql'
+    @sparql = SPARQL.parse(File.read(fix_date_sparql_file), update: true)
   end
 
   # check that the dates are correctly formatted
   def test_date_format
-    sparql = SPARQL.parse(File.read(@fix_date_sparql_file), update: true)
     graph = RDF::Graph.load("./tests/fixtures/test_date.jsonld")
     # puts "before: #{graph.dump(:jsonld)}"
-    graph.query(sparql)
+    graph.query(@sparql)
     # puts "after: #{graph.dump(:jsonld)}"
     dates = graph.query([nil, RDF::Vocab::SCHEMA.startDate, nil]).each.objects
     dates.each do |start_date|
@@ -21,15 +21,24 @@ class DateTest < Minitest::Test
   end
 
   def test_date_type
-    sparql = SPARQL.parse(File.read(@fix_date_sparql_file), update: true)
     graph = RDF::Graph.load("./tests/fixtures/test_date_types.jsonld")
     # puts "before: #{graph.dump(:jsonld)}"
-    graph.query(sparql)
+    graph.query(@sparql)
     # puts "after: #{graph.dump(:jsonld)}"
     events = JSON.parse(graph.dump(:jsonld))
     events.each do |event|
       start_date = event['http://schema.org/startDate']
       assert_equal(start_date[0]["@type"], "http://schema.org/Date")
     end
+  end
+
+  # Check that startDate retains its data type after being fixed
+  def test_date_type_in_context
+    graph = RDF::Graph.load("./tests/fixtures/test_date_type_in_context.jsonld")
+    # get the startDate data type before fixing
+    expected = graph.query([nil, RDF::Vocab::SCHEMA.startDate, nil]).first.object.datatype
+    graph.query(@sparql)
+    assert_equal expected, graph.query([nil, RDF::Vocab::SCHEMA.startDate, nil]).first.object.datatype, 
+      "Data type of startDate should be preserved after fixing"
   end
 end
