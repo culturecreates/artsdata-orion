beb6d927e7b1c39a222717b4b5f743ee20766ef1
dev <dev.aravind@culturecreates.com>
2024-08-26 13:42:34 +0530

    Updates orion to add headless support

--- src/lib/headless_browser.rb
+++ src/lib/headless_browser.rb
@@ -9,7 +9,8 @@ module HeadlessBrowser
     entity_urls.each do |entity_url|
       begin
         browser.go_to(entity_url)
-        browser.network.wait_for_idle(duration: 1, timeout: 60.0)
+        sleep 15
+        browser.stop
         json_ld_scripts = browser.css("script[type='application/ld+json']")
         json_ld_scripts.each do |script|
           begin
