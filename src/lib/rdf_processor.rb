c9af324bf2938f2cad22dd08b2b43eaad8aa7818
dev <dev.aravind@culturecreates.com>
2024-10-16 17:27:19 +0530

    Updated user agent in RDF.rb

--- src/lib/rdf_processor.rb
+++ src/lib/rdf_processor.rb
@@ -1,6 +1,7 @@
 require 'linkeddata'
 require 'sparql'
 require_relative 'sparql_processor'
+require 'rubygems'
 module RDFProcessor
   def self.process_rdf(entity_urls, base_url)
     graph = RDF::Graph.new
@@ -10,7 +11,9 @@ module RDFProcessor
       begin
         puts "Processing #{entity_url} in non-headless mode"
         entity_url = entity_url.gsub(' ', '+')
-        loaded_graph = RDF::Graph.load(entity_url)
+        linkeddata_version = Gem::Specification.find_by_name('linkeddata').version.to_s
+        options = { headers: { 'User-Agent' => "artsdata-crawler/#{linkeddata_version}" } }
+        loaded_graph = RDF::Graph.load(entity_url, **options)
         sparql_file_with_url = add_url_sparql_file.gsub("subject_url", entity_url)
         loaded_graph.query(SPARQL.parse(sparql_file_with_url, update: true))
         graph << loaded_graph
