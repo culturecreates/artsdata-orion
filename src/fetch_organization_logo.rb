35871a1bbb5e0460529aac4d742bca4c639c7f43
dev <dev.aravind@culturecreates.com>
2025-09-04 16:29:25 +0530

    fix(organization-logo): fixes to logo urls, checks more types

--- src/fetch_organization_logo.rb
+++ src/fetch_organization_logo.rb
@@ -3,11 +3,42 @@ require 'nokogiri'
 require 'open-uri'
 
 BASE_URL = "https://staging.recon.artsdata.ca/extend/http%3A%2F%2Fkg.artsdata.ca%2Fcore/Organization"
+
+# rdfs:subClassOf* is not working
+ORGANIZATION_SUBTYPES = [
+  RDF::Vocab::SCHEMA.Airline,
+  RDF::Vocab::SCHEMA.Consortium,
+  RDF::Vocab::SCHEMA.Cooperative,
+  RDF::Vocab::SCHEMA.Corporation,
+  RDF::Vocab::SCHEMA.EducationalOrganization,
+  RDF::Vocab::SCHEMA.FundingScheme,
+  RDF::Vocab::SCHEMA.GovernmentOrganization,
+  RDF::Vocab::SCHEMA.LibrarySystem,
+  RDF::Vocab::SCHEMA.LocalBusiness,
+  RDF::Vocab::SCHEMA.MedicalOrganization,
+  RDF::Vocab::SCHEMA.NGO,
+  RDF::Vocab::SCHEMA.NewsMediaOrganization,
+  RDF::Vocab::SCHEMA.OnlineBusiness,
+  RDF::Vocab::SCHEMA.PerformingGroup,
+  RDF::Vocab::SCHEMA.PoliticalParty,
+  RDF::Vocab::SCHEMA.Project,
+  RDF::Vocab::SCHEMA.ResearchOrganization,
+  RDF::Vocab::SCHEMA.SearchRescueOrganization,
+  RDF::Vocab::SCHEMA.SportsOrganization,
+  RDF::Vocab::SCHEMA.WorkersUnion,
+  RDF::Vocab::SCHEMA.Organization
+]
+
+PARENT_TYPES =[
+  RDF::Vocab::SCHEMA.WebPage, 
+  RDF::Vocab::SCHEMA.WebSite
+]
+
 $organization_with_logo_count = 0 
 $error_count = 0
 
 
-def fetch_orgs_with_url(limit = 200)
+def fetch_orgs_with_url(limit = 1000)
   page = 1
   orgs_with_url = []
   retries = 3
@@ -58,21 +89,64 @@ def fetch_rdfa(homepage_url)
   graph
 end
 
-def extract_logo(graph, org_uri)
-  logos = []
+def extract_logos(graph, organization)
+  logo_objects = graph.query([organization, RDF::Vocab::SCHEMA.logo, nil]).map(&:object)
+  image_objects = graph.query([organization, RDF::Vocab::SCHEMA.image, nil]).map(&:object)
+  logos_and_images = logo_objects + image_objects
 
-  logos += graph.query([nil, RDF::Vocab::SCHEMA.image, nil]).map(&:object)
+  logos = logos_and_images.map do |logo|
+    if logo.literal?
+      RDF::URI(logo.value)
+    elsif logo.uri?
+      url = graph.query([logo, RDF::Vocab::SCHEMA.url, nil]).objects.first
+      url || logo    
+    else
+      graph.query([logo, RDF::Vocab::SCHEMA.url, nil]).objects.first
+    end
+  end.compact.uniq
 
-  logos += graph.query([nil, RDF::Vocab::SCHEMA.logo, nil]).map(&:object)
+  logos
+end
 
-  graph.query([nil, RDF::Vocab::SCHEMA.image, nil]).each do |stmt|
-    img_obj = stmt.object
-    graph.query([img_obj, RDF::Vocab::SCHEMA.url, nil]).each do |url_stmt|
-      logos << url_stmt.object
+def extract_types(graph, organization)
+  graph.query([organization, RDF.type, nil]).map(&:object)
+end
+
+def get_top_level_organizations(graph)
+  entities = ORGANIZATION_SUBTYPES.flat_map do |type|
+    graph.query([nil, RDF.type, type]).map(&:subject)
+  end.uniq
+  entities.select { |entity| graph.query([nil, nil, entity]).empty? }
+end
+
+def get_embedded_organizations(graph)
+  organizations = []
+
+  PARENT_TYPES.each do |parent_type|
+    graph.query([nil, RDF.type, parent_type]).each do |stmt|
+      parent = stmt.subject
+
+      graph.query([parent, nil, nil]).each do |sub_stmt|
+        sub_entity = sub_stmt.object
+
+        next unless sub_entity.is_a?(RDF::URI)
+
+        if ORGANIZATION_SUBTYPES.any? { |t| graph.query([sub_entity, RDF.type, t]).to_a.any? }
+          organizations << sub_entity
+        end
+      end
     end
   end
 
-  logos.uniq
+  organizations.uniq
+end
+
+def fetch_organizations_from_graph(graph)
+  top_level_organizations = get_top_level_organizations(graph)
+  puts "Found #{top_level_organizations.count} top level organizations"
+  embedded_organizaions = get_embedded_organizations(graph)
+  puts "Found #{embedded_organizaions.count} embedded organizations"
+  (top_level_organizations + embedded_organizaions).uniq
 end
 
 def build_graph(orgs)
@@ -82,26 +156,37 @@ def build_graph(orgs)
     homepage_url = org['url']
     artsdata_uri = RDF::URI(org['uri'])
     organization_uri = RDF::URI(homepage_url + "#Organization")
-    organization_type = org['type'].split(',') || RDF::Vocab::SCHEMA.Organization
     puts "Fetching page #{index + 1}/#{orgs.size}, url: #{homepage_url}"
 
     rdfa_graph = fetch_rdfa(homepage_url)
+    begin
+      organizations = fetch_organizations_from_graph(rdfa_graph)
+      for organization in organizations
 
-    logos = extract_logo(rdfa_graph, RDF::URI(homepage_url))
-    next if logos.empty?
+        logos = extract_logos(rdfa_graph, organization)
+        types = extract_types(rdfa_graph, organization)
 
-    puts "Found #{logos.size} logos for #{homepage_url}"
-    $organization_with_logo_count += 1
+        if logos.empty?
+          puts "Skipping as no logos exist"
+          next
+        end
 
-    organization_type.each do |type|
-      output_graph << [organization_uri, RDF.type, RDF::URI(type.strip())]
-    end    
-    output_graph << [organization_uri, RDF::Vocab::SCHEMA.url, RDF::URI(homepage_url)]
-    output_graph << [organization_uri, RDF::Vocab::SCHEMA.name, org['name']]
-    output_graph << [organization_uri, RDF::Vocab::SCHEMA.sameAs, artsdata_uri]
+        puts "Found #{logos.size} logos for #{homepage_url} with types #{types.map(&:to_s).join(', ')}"
+        $organization_with_logo_count += 1  
+        output_graph << [organization_uri, RDF::Vocab::PROV.wasDerivedFrom, RDF::URI(homepage_url)]
+        types.each do |t|
+          output_graph << [organization_uri, RDF.type, t]
+        end        
+        output_graph << [organization_uri, RDF::Vocab::SCHEMA.name, org['name']]
+        output_graph << [organization_uri, RDF::Vocab::SCHEMA.sameAs, artsdata_uri]
 
-    logos.each do |logo|
-      output_graph << [organization_uri, RDF::Vocab::SCHEMA.logo, logo]
+        logos.each do |logo|
+          output_graph << [organization_uri, RDF::Vocab::SCHEMA.logo, logo]
+        end
+      end
+    rescue StandardError => e
+      warn "Error extracting logos from RDFa graph for #{homepage_url}: #{e.message}"
+      next
     end
   end
 
@@ -109,6 +194,7 @@ def build_graph(orgs)
 end
 
 orgs_with_url = fetch_orgs_with_url()
+orgs_with_url = orgs_with_url.first(100)
 graph = build_graph(orgs_with_url)
 File.open("output/organization_logo.jsonld", 'w') do |file|
   file.puts(graph.dump(:jsonld))
